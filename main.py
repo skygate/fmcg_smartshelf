1f14208e692decb3780f2e4d08f91f36b484dcfe
wojciechczarnecki <wczarnecky@gmail.com>
2020-06-16 17:09:17 +0200

    Add first version of PoC

---
+++ main.py
@@ -0,0 +1,223 @@
+import argparse
+import os
+from typing import List, Tuple, Dict
+
+import cv2
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.autograd import Variable
+from torchvision.models import ResNet
+
+from config import (
+    CLASSIFIERS_PATH,
+    DETECTOR_CFG,
+    DETECTOR_WEIGHTS,
+    RESOLUTION,
+    MEAN,
+    STD,
+    DATA_PATH,
+)
+
+
+class Runner:
+    """
+    Class that runs the system sequence steps
+    """
+
+    def __init__(self, image_path: str) -> None:
+        self.image_path = image_path
+        self.classifier = Classifier(image_path)
+        self.drawer = Drawer(image_path)
+        self.detector = Detector(image_path)
+
+    def run(self) -> None:
+        object_types = ["plate", "flat"]
+        object_classification_result = self.classifier.make_classification(
+            "object_classifier.pth", object_types
+        )
+
+        states = ["good", "defective"]
+        models = {
+            "plate": self.classifier.make_classification("plate_state.pth", states),
+            "flat": self.classifier.make_classification("flat_state.pth", states),
+        }
+        state_classification_result = models[object_classification_result]
+
+        self.drawer.put_text(object_classification_result, state_classification_result)
+
+        defect_types = ["scratches", "recess"]
+        if state_classification_result == "defective":
+            bboxes = self.detector.get_bboxes()
+            self.drawer.draw_bboxes(bboxes, defect_types)
+
+        cv2.imshow("Image", self.drawer.image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+class Classifier:
+    """
+    Class that makes classification of the image using trained model
+    """
+
+    def __init__(self, image_path: str) -> None:
+        self.device: torch.device = torch.device(
+            "cuda:0" if torch.cuda.is_available() else "cpu"
+        )
+        self.image_path = image_path
+        self.image_tensor = self._convert_image_to_tensor()
+
+    def _convert_image_to_tensor(self) -> torch.Tensor:
+        preprocess = self._preprocess_image()
+        image_tensor = preprocess(Image.open(self.image_path)).float()
+        return image_tensor.unsqueeze_(0)
+
+    def _preprocess_image(self) -> transforms.Compose:
+        compose_transforms = [
+            transforms.Resize(RESOLUTION),
+            transforms.ToTensor(),
+            transforms.Normalize(np.asarray(MEAN), np.asarray(STD)),
+        ]
+        return transforms.Compose(compose_transforms)
+
+    def make_classification(self, model_name: str, classes: List[str]) -> str:
+        model: ResNet = torch.load(
+            os.path.join(CLASSIFIERS_PATH, model_name), map_location=self.device
+        )
+        model.to(self.device).eval()
+        fc_out = model(Variable(self.image_tensor))
+        output = fc_out.detach().numpy()
+        return classes[output.argmax()]
+
+
+class Detector:
+    """
+    Class that detects defects on image using trained model
+    """
+
+    def __init__(self, image_path: str) -> None:
+        self.image = cv2.imread(image_path)
+        self.image = cv2.resize(self.image, (1920, 1080))
+        self.image_width = self.image.shape[1]
+        self.image_height = self.image.shape[0]
+        self.model = cv2.dnn.readNet(DETECTOR_CFG, DETECTOR_WEIGHTS)
+        self.confidences = None
+        self.boxes = None
+        self.classes = None
+
+    def get_bboxes(self) -> Dict[Tuple[int, int, int, int], int]:
+        indices = self._remove_high_overlapping_boxes()
+        bboxes = {}
+        for idx in indices:
+            idx = idx[0]
+            x, y, w, h = self.boxes[idx]
+            box = round(x), round(y), round(w), round(h)
+            bboxes[box] = self.classes[idx]
+        return bboxes
+
+    def _remove_high_overlapping_boxes(self) -> np.ndarray:
+        self._get_detections()
+        return cv2.dnn.NMSBoxes(self.boxes, self.confidences, 0.5, 0.4)
+
+    def _get_detections(self) -> None:
+        self.confidences = []
+        self.boxes = []
+        self.classes = []
+        outs = self._detect_objects()
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    center_x = int(detection[0] * self.image_width)
+                    center_y = int(detection[1] * self.image_height)
+                    w = int(detection[2] * self.image_width)
+                    h = int(detection[3] * self.image_height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    self.confidences.append(float(confidence))
+                    self.boxes.append([x, y, w, h])
+                    self.classes.append(class_id)
+
+    def _detect_objects(self) -> List[List[List[float]]]:
+        image_blob = self._preprocess()
+        self.model.setInput(image_blob)
+        return self.model.forward(self._get_output_layers())
+
+    def _preprocess(self) -> np.ndarray:
+        return cv2.dnn.blobFromImage(
+            self.image, 0.004, (416, 416), (0, 0, 0), True, crop=False
+        )
+
+    def _get_output_layers(self) -> List[str]:
+        layer_names = self.model.getLayerNames()
+        return [layer_names[i[0] - 1] for i in self.model.getUnconnectedOutLayers()]
+
+
+class Drawer:
+    """
+    Class that draws on the given image
+    """
+
+    def __init__(self, image_path: str) -> None:
+        self.image = cv2.imread(image_path)
+        self.image = cv2.resize(self.image, (1920, 1080))
+
+    def put_text(self, object_type: str, state: str) -> None:
+        cv2.putText(
+            self.image,
+            f"Object: {object_type}",
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 255),
+            2,
+        )
+        color = (0, 255, 0) if state == "good" else (0, 0, 255)
+        cv2.putText(
+            self.image,
+            f"State: {state}",
+            (10, 75),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            color,
+            2,
+        )
+
+    def draw_bboxes(
+        self, bboxes: Dict[Tuple[int, int, int, int], int], classes: List[str]
+    ) -> None:
+        for bbox, label in bboxes.items():
+            p1 = (bbox[0], bbox[1])
+            p2 = (bbox[0] + bbox[2], bbox[1] + bbox[3])
+            color = (255, 255, 0) if label == 0 else (255, 255, 255)
+            cv2.rectangle(self.image, p1, p2, color, 2)
+            cv2.putText(
+                self.image,
+                classes[label],
+                (bbox[0], bbox[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                color,
+                2,
+            )
+
+
+def main(args: argparse.Namespace) -> None:
+    image_path = os.path.join(DATA_PATH, args.name)
+    runner = Runner(image_path)
+    runner.run()
+
+
+def set_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Air motor Prove of Concept")
+    parser.add_argument("-n", "--name", required=True, help="Name of the image")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    main(set_args())
