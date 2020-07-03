975af73764f8d9da436cde9ee139ebe7d7979b0f
Martyna Matusek <martynamatusek@MBP-Michal.skygate.io>
2020-07-03 14:38:02 +0200

    [TASK-build-frontend] add global styles

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 import Main from "../pages/Main/Main";
 import Statistics from "../pages/Statictics";
 import { Navigation } from "../components/Navigation";
+import { fontFamily } from "../styles/variable";
+
+const GlobalStyle = createGlobalStyle`
+ body{
+    font-family:${fontFamily.spartan};  
+ }
+  
+`;
 
 export const Routes = ({}) => {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Switch>
-        <Route exact path={"/"}>
-          <Main />
-        </Route>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path={"/"}>
+            <Main />
+          </Route>
 
-        <Route exact path={"/statistics"}>
-          <Statistics />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+          <Route exact path={"/statistics"}>
+            <Statistics />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
