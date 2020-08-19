d72089f568345e2693add0cee8d71f12185e8da2
szymon-konieczny <szykon@gmail.com>
2020-08-19 13:26:02 +0200

    Add product name to history result

--- backend/scripts/get_history_box_response.py
+++ backend/scripts/get_history_box_response.py
@@ -1,11 +1,18 @@
+import sys
+sys.path.append('/Users/skygate/Projects/shelf-analytics')
+
+from backend.db.db import Box
+
+
 def get_history_box_response(history):
     response = []
     
     for box_history in history:
         box_history_config = {
             'boxId': box_history.box_id,
+            'productName': box_history.box.product_name,
             'timestamp': box_history.timestamp,
-            'state': box_history.state
+            'state': box_history.state,
         }
         response.append(box_history_config)
         
--- backend/services/database_service.py
+++ backend/services/database_service.py
@@ -3,19 +3,23 @@ sys.path.append('/Users/skygate/Projects/shelf-analytics')
 
 from backend.db.db import Box, History
 
+
 class DatabaseService:
     def get_boxes(self):
         return list(Box.select())
-    
+
+    def get_box_by_id(self, box_id):
+        return Box.select().where(Box.box_id == box_id)
+
     def save_box_classification(self, timestamp, state, box):
         History(timestamp = timestamp, state = state, box = box).save()
-        
-    def get_history(self): 
-        return list(History.select())
-    
+
+    def get_history(self):
+        return list(History.select().join(Box))
+
     def get_history_by_date(self, start_date, end_date):
         return list(History.select().where(History.timestamp.between(start_date, end_date)))
-    
+
     def get_history_by_id(self, box_id):
         return list(History.select().where(History.box_id == box_id))
         
